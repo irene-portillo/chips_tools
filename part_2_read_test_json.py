85675d2a386440e9d8babccf3032f95bf9fe7e35
irene-portillo <irenemaideli@gmail.com>
2024-02-15 18:13:02 -0500

    Working with JSON - Pt 1
    Went through majority of "Working with JSON" but ran into issue, will resolve later

--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -3,18 +3,27 @@ import json
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
 def make_game_library_from_json( json_data ):
-    #Initialize a new GameLibrary
+    
+    #Initialize a new GameLibrary - aka create GameLibrary obj
     game_library = test_data.GameLibrary()
 
-    ### Begin Add Code Here ###
-    #Loop through the json_data
-        #Create a new Game object from the json_data by reading
-        #  title
-        #  year
-        #  platform (which requires reading name and launch_year)
-        #Add that Game object to the game_library
-    ### End Add Code Here ###
+    print('code running...')
 
+    #Loop through the json_data
+    for game in json_data: # shouldn't this be looping through test_data since it holds the games?
+        #Create a new Game object from the json_data by reading
+        print(game)
+
+        #Get platform info seperately (which requires reading name and launch_year)
+        launch_yr = game["platform"]["launch year"]
+        plat_name = game["platform"]["name"]
+        curr_plat = test_data.Platform(plat_name, launch_yr) 
+        #Create the game obj
+        new_game_obj = test_data.Game( game["title"], curr_plat, game["year"])
+        #Add that Game object to the game_library
+        game_library.add_game(new_game_obj)
+    
+    ### End Add Code Here ### -- not sure what's supposed to go on here
     return game_library
 
 
@@ -27,3 +36,10 @@ input_json_file = "data/test_data.json"
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
 #Print out the resulting GameLibrary data using print()
 ### End Add Code Here ###
+
+#Open the JSON file
+with open(input_json_file, "r") as reader:
+    #load the JSON data and store it in the variable family_json_data
+    json_data = json.load(reader)
+    test_var = make_game_library_from_json(json_data)
+    print(test_var)
