fcb45a16b61b0f237769aaa5b63afa79bc323404
irene-portillo <irenemaideli@gmail.com>
2024-02-20 11:30:12 -0500

    Set up/fixed Level Library to fit cc_classes.py
    - Revised json for monster/movingObjs
    - deleted own_levels.py since it was the same as cc_classes.py which had levels inside
    -created own_levels.dat
    - Updated my pt3 file to adjust to the cc_classes.py

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -5,40 +5,57 @@ import cc_dat_utils
 #Convert JSON data to CCLevelPack
 #Save converted data to DAT file
 
-
-
-### Load custom JSON File --------------------------- CODE FROM PAST FILE, pt 2
-import own_levels
+### Load custom JSON File
+import cc_classes
 import json
+
+# Create CC Coordinates 
+def createCoordinates(movingObsList):
+    listForField = []
+    for monsterList in movingObsList:
+        newCoord = cc_classes.CCCoordinate( monsterList[0], monsterList[1] )
+        listForField.append(newCoord)
+    return listForField
+
+# Create optional fields
+def make_optional_fields(level, newLevel):        
+    #Title field
+    title = level["optional_fields"]["title"] 
+    titleField = cc_classes.CCMapTitleField(title)
+    #Password field 
+    password = level["optional_fields"]["password"]
+    passField = cc_classes.CCEncodedPasswordField(password)
+    # Hint field
+    hint = level["optional_fields"]["hint"]
+    hintField = cc_classes.CCMapHintField(hint)
+    # Moving objs field
+    movingObjs = level["optional_fields"]["moving_objs"] # starting pos
+    movingObjsField = cc_classes.CCMonsterMovementField( createCoordinates(movingObjs))
+    # SET optional fields
+    newLevel.optional_fields = [titleField,passField, hintField, movingObjsField]
 
 #Creates and returns a LevelLibrary object(defined in own_levels) from loaded json_data
 def make_level_library_from_json( json_data ):
-    
-    #Initialize a new LevelLibrary - aka create LevelLibrary obj
-    level_library = own_levels.LevelLibrary()
+    #Initialize a new LevelLibrary 
+    level_library = cc_classes.CCLevelPack()
 
     #Loop through the json_data
     for level in json_data["Levels"]: 
+        #Initialize a new level obj
+        newLevel = cc_classes.CCLevel()
         #Create a new level object from the json_data by reading
-        num = level["level_num"]
-        timeLim = level["time_lim"]
-        chips = level["chip_num"]
-        upperLayer = level["upper_layer"]
-        lowerLayer = level["lower_layer"]
-        #Optional fields
-        title = level["optional_fields"]["title"]
-        password = level["optional_fields"]["password"]
-        hint = level["optional_fields"]["hint"]
-        movingObjs = level["optional_fields"]["moving_objs"]
-
-        #Create the level obj
-        new_level_obj = own_levels.Level(num, timeLim, chips, upperLayer, lowerLayer, title, password, hint, movingObjs)
-        
+        newLevel.level_number = level["level_num"]
+        newLevel.time = level["time_lim"]
+        newLevel.num_chips = level["chip_num"]
+        newLevel.upper_layer = level["upper_layer"]
+        newLevel.lower_layer = level["lower_layer"]
+        make_optional_fields(level, newLevel)
         #Add that level object to the level_library
-        level_library.add_level(new_level_obj)
+        level_library.add_level(newLevel)
+    
+    # print(level_library)
+    return level_library #Return -> Converted JSON data to CCLevelPack 
 
-
-    return level_library
 
 #Assign file
 input_json_file = "data/own_levels.json"
@@ -47,14 +64,16 @@ input_json_file = "data/own_levels.json"
 with open(input_json_file, "r") as reader:
     #load the JSON data and store it in the variable family_json_data
     json_data = json.load(reader)
-    var = make_level_library_from_json(json_data)
-    print(var)
+    levelLibrary = make_level_library_from_json(json_data)
+    print('Done loading json files! Created levelLibrary')
 
-print('Done loading json files!')
+    # make ccLevelPack into a dat file
+    datFile = cc_dat_utils.write_cc_level_pack_to_dat(levelLibrary, "own_levels.dat")
+    print(datFile)
+    print('created dat file')
 
-#Convert JSON data to CCLevelPack -----------------------------------------CODE FROM PAST FILE, pt 1 
-input_dat_file = "data/own_levels.dat"## how do i make this???????????
-
-#Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
-# level = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file) 
-# print(level)
+    #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file - Used from pt1
+    input_dat_file = "data/own_levels.dat"
+    attemptLevel = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file) 
+    # print(attemptLevel)
+    print("END--------------------------------------------------------------------------------------------------")
